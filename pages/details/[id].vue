f99a3cd5812e669957e5b9bf449ba18c17123d22
SethyRung <rungsethyhk@gmail.com>
2025-02-25 17:09:40 +0700

    feat: enhance loading experience with skeleton components in news details view

--- pages/details/[id].vue
+++ pages/details/[id].vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="max-w-[1280px] mx-auto p-4 space-y-4">
+  <div
+    class="max-w-[1280px] min-h-[calc(100vh_-_202px)] computer:min-h-[calc(100vh_-_166px)] mx-auto p-4 space-y-4"
+  >
     <div
       class="grid grid-cols-1 computer:grid-cols-[70%_1fr] grid-rows-[auto_auto_1fr_auto] computer:grid-rows-[auto_auto_1fr] gap-8"
     >
       <div class="space-y-6 col-start-1 col-end-2 row-start-1 row-end-2">
         <p>{{ data?.news.category.name }}</p>
+        <USkeleton v-if="isLoading" class="h-4 w-20" />
+
         <h1 class="font-bold text-2xl computer:text-4xl">
           {{ data?.news.title }}
         </h1>
+        <div v-if="isLoading" class="space-y-3">
+          <USkeleton class="h-6 w-full" />
+          <USkeleton class="h-6 w-[70%]" />
+          <USkeleton class="h-6 w-[80%]" />
+        </div>
+
         <div class="space-y-2">
           <p class="text-xs">
             By {{ data?.news.author.first_name }}
             {{ data?.news.author.last_name }}
           </p>
+
           <p class="text-xs">
             <UIcon name="i-lucide-calendar-days" />
             {{ dayjs(data?.news.date_published) }}
@@ -24,10 +35,23 @@
         provider="directus"
         class="w-full col-start-1 col-end-2 row-start-2 row-end-3"
       />
+      <USkeleton
+        v-if="isLoading"
+        class="w-full h-56 computer:h-96 col-start-1 col-end-2 row-start-2 row-end-3"
+      />
       <div
         v-dompurify-html="data?.news.content"
         class="news-content col-start-1 col-end-2 row-start-3 row-end-4"
       ></div>
+      <div
+        v-if="isLoading"
+        class="col-start-1 col-end-2 row-start-3 row-end-4 space-y-3"
+      >
+        <USkeleton class="w-full h-4" />
+        <USkeleton class="w-[80%] h-4" />
+        <USkeleton class="w-[70%] h-4" />
+        <USkeleton class="w-[90%] h-4" />
+      </div>
       <div
         class="space-y-3 col-start-1 col-end-2 computer:col-start-2 computer:col-end-3 row-start-4 row-end-5 computer:row-start-2 computer:row-end-4"
       >
@@ -46,6 +70,24 @@
           }"
           :to="`/details/${news.id}`"
         />
+        <template v-if="isLoading">
+          <div
+            v-for="i in Array(3)"
+            :key="i"
+            class="w-full h-36 grid grid-rows-1 grid-cols-[40%_1fr] gap-2"
+          >
+            <USkeleton class="size-full" />
+            <div>
+              <USkeleton class="h-5 w-full" />
+              <div class="mt-6 space-y-2">
+                <USkeleton class="h-3 w-[90%]" />
+                <USkeleton class="h-3 w-[70%]" />
+                <USkeleton class="h-3 w-[80%]" />
+                <USkeleton class="h-3 w-full" />
+              </div>
+            </div>
+          </div>
+        </template>
       </div>
     </div>
   </div>
@@ -58,38 +100,48 @@ import dayjs from "dayjs";
 const config = useRuntimeConfig();
 const { path, params } = useRoute();
 const { getItemById, getItems } = useDirectusItems();
-const { data } = useAsyncData(path, async () => {
-  const news = await getItemById<News>({
-    collection: "news",
-    id: params.id as string,
-    params: {
-      fields: ["*", "author.*", "category.*"],
-    },
-  });
-
-  const moreNews = await getItems<News>({
-    collection: "news",
-    params: {
-      fields: ["id", "title", "featured_image"],
-      filter: {
-        _and: [
-          {
-            id: {
-              _neq: news.id,
-            },
-          },
-          {
-            category: {
-              _eq: news.category.id,
-            },
-          },
-        ],
+const { data, status, execute } = useAsyncData(
+  path,
+  async () => {
+    const news = await getItemById<News>({
+      collection: "news",
+      id: params.id as string,
+      params: {
+        fields: ["*", "author.*", "category.*"],
       },
-      limit: 3,
-    },
-  });
+    });
 
-  return { news, moreNews };
+    const moreNews = await getItems<News>({
+      collection: "news",
+      params: {
+        fields: ["id", "title", "featured_image"],
+        filter: {
+          _and: [
+            {
+              id: {
+                _neq: news.id,
+              },
+            },
+            {
+              category: {
+                _eq: news.category.id,
+              },
+            },
+          ],
+        },
+        limit: 3,
+      },
+    });
+
+    return { news, moreNews };
+  },
+  { lazy: true, immediate: false },
+);
+
+const isLoading = computed(() => status.value !== "success");
+
+onMounted(() => {
+  execute();
 });
 </script>
 <style scoped>
