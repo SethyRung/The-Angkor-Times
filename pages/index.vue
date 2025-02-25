ee67b6f6306eed9b9e9c95899dfc3ed72517e96e
SethyRung <rungsethyhk@gmail.com>
2025-02-25 16:20:17 +0700

    feat: enhance news loading experience with skeleton loaders and increase search result limit

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,7 @@
         class="h-full overflow-hidden flex flex-col-reverse computer:flex-row gap-4"
       >
         <div
-          v-if="recentNews.length > 0"
-          class="computer:max-w-[240px] h-full my-4 computer:my-0 space-y-3"
+          class="computer:w-[240px] flex-shrink-0 my-4 computer:my-0 space-y-3"
         >
           <h2 class="font-bold underline font-red-hat-display">Recent News</h2>
           <div class="flex flex-col items-center gap-3">
@@ -26,13 +25,44 @@
               </NuxtLink>
               <div
                 v-if="index !== recentNews.length - 1"
-                class="w-[80%] border border-[var(--ui-border)]"
+                class="w-[80%] border border-neutral-300"
               ></div>
             </template>
+            <template v-if="isLoading"
+              ><div class="w-full space-y-2">
+                <USkeleton class="h-5 w-full" />
+                <USkeleton class="h-3 w-[90%]" />
+                <USkeleton class="h-3 w-[70%]" />
+                <USkeleton class="h-3 w-[80%]" />
+                <USkeleton class="h-3 w-36" />
+              </div>
+              <div class="w-[80%] border border-neutral-300"></div>
+              <div class="w-full space-y-2">
+                <USkeleton class="h-5 w-full" />
+                <USkeleton class="h-3 w-[90%]" />
+                <USkeleton class="h-3 w-[70%]" />
+                <USkeleton class="h-3 w-[80%]" />
+                <USkeleton class="h-3 w-36" />
+              </div>
+              <div class="w-[80%] border border-neutral-300"></div>
+              <div class="w-full space-y-2">
+                <USkeleton class="h-5 w-full" />
+                <USkeleton class="h-3 w-[90%]" />
+                <USkeleton class="h-3 w-[70%]" />
+                <USkeleton class="h-3 w-[80%]" />
+                <USkeleton class="h-3 w-36" /></div
+            ></template>
           </div>
         </div>
         <div
-          class="h-full computer:px-2 grid grid-rows-2 computer:grid-rows-[70%_1fr] gap-2 computer:border-x-2 border-neutral-300 space-y-4"
+          :class="
+            clsx(
+              'grow h-full computer:px-2 gap-2 computer:border-x-2 border-neutral-300 space-y-4',
+              isLoading
+                ? 'flex flex-col'
+                : 'grid grid-rows-2 computer:grid-rows-[70%_1fr]',
+            )
+          "
         >
           <NewsCard
             v-if="heroNews"
@@ -41,6 +71,13 @@
             :description="heroNews.description"
             :to="`/details/${heroNews.id}`"
           />
+          <div v-if="isLoading" class="w-full space-y-2">
+            <USkeleton class="w-full h-56" />
+            <USkeleton class="h-5 w-full" />
+            <USkeleton class="h-3 w-[90%]" />
+            <USkeleton class="h-3 w-[70%]" />
+            <USkeleton class="h-3 w-[80%]" />
+          </div>
           <NewsCard
             v-if="headerNews.length > 1"
             :image="headerNews[0].featured_image"
@@ -51,32 +88,59 @@
             }"
             :to="`/details/${headerNews[0].id}`"
           />
+          <div
+            v-if="isLoading"
+            class="h-full w-full grid grid-rows-[144px_1fr] grid-cols-1 tablet:h-56 tablet:grid-rows-1 tablet:grid-cols-2 computer:grid-cols-[326px_1fr] gap-2"
+          >
+            <USkeleton class="size-full" />
+            <div class="space-y-2">
+              <USkeleton class="h-5 w-full" />
+              <USkeleton class="h-3 w-[90%]" />
+              <USkeleton class="h-3 w-[70%]" />
+              <USkeleton class="h-3 w-[80%]" />
+            </div>
+          </div>
         </div>
       </div>
-      <div
-        v-if="headerNews.length === 3"
-        class="h-full flex flex-col overflow-hidden gap-4"
-      >
-        <NewsCard
-          :image="headerNews[1].featured_image"
-          :title="headerNews[1].title"
-          :description="headerNews[1].description"
-          :ui="{
-            title: 'text-sm',
-            description: 'line-clamp-3 break-all',
-          }"
-          :to="`/details/${headerNews[1].id}`"
-        />
-        <NewsCard
-          :image="headerNews[2].featured_image"
-          :title="headerNews[2].title"
-          :description="headerNews[2].description"
-          :ui="{
-            title: 'text-sm',
-            description: 'line-clamp-3 break-all',
-          }"
-          :to="`/details/${headerNews[2].id}`"
-        />
+      <div class="h-full flex flex-col overflow-hidden gap-4">
+        <template v-if="isLoading">
+          <div class="w-full space-y-2">
+            <USkeleton class="w-full h-36" />
+            <USkeleton class="h-5 w-full" />
+            <USkeleton class="h-3 w-[90%]" />
+            <USkeleton class="h-3 w-[70%]" />
+            <USkeleton class="h-3 w-[80%]" />
+          </div>
+          <div class="w-full space-y-2">
+            <USkeleton class="w-full h-36" />
+            <USkeleton class="h-5 w-full" />
+            <USkeleton class="h-3 w-[90%]" />
+            <USkeleton class="h-3 w-[70%]" />
+            <USkeleton class="h-3 w-[80%]" />
+          </div>
+        </template>
+        <template v-if="headerNews.length === 3">
+          <NewsCard
+            :image="headerNews[1].featured_image"
+            :title="headerNews[1].title"
+            :description="headerNews[1].description"
+            :ui="{
+              title: 'text-sm',
+              description: 'line-clamp-3 break-all',
+            }"
+            :to="`/details/${headerNews[1].id}`"
+          />
+          <NewsCard
+            :image="headerNews[2].featured_image"
+            :title="headerNews[2].title"
+            :description="headerNews[2].description"
+            :ui="{
+              title: 'text-sm',
+              description: 'line-clamp-3 break-all',
+            }"
+            :to="`/details/${headerNews[2].id}`"
+          />
+        </template>
       </div>
     </div>
     <div
@@ -108,6 +172,7 @@
 </template>
 
 <script lang="ts" setup>
+import clsx from "clsx";
 import type { Category } from "~/types/Category";
 import type { News } from "~/types/News";
 const { path } = useRoute();
@@ -156,32 +221,45 @@ const processNewsData = (
 };
 
 const { getItems } = useDirectusItems();
-const { data, status } = await useAsyncData(path, async () => {
-  return {
-    news: await getItems<News>({
-      collection: "news",
-      params: {
-        fields: [
-          "id",
-          "title",
-          "description",
-          "featured_image",
-          "date_published",
-          "category.*",
-        ],
-      },
-    }),
-    categories: await getItems<Category>({ collection: "categories" }),
-  };
+const { data, status, execute } = await useAsyncData(
+  path,
+  async () => {
+    return {
+      news: await getItems<News>({
+        collection: "news",
+        params: {
+          fields: [
+            "id",
+            "title",
+            "description",
+            "featured_image",
+            "date_published",
+            "category.*",
+          ],
+        },
+      }),
+      categories: await getItems<Category>({ collection: "categories" }),
+    };
+  },
+  { lazy: true, immediate: false },
+);
+
+watch(status, (value) => {
+  if (value === "success" && data.value) {
+    const processedData = processNewsData(
+      data.value.categories,
+      data.value.news,
+    );
+    heroNews.value = processedData.heroNews;
+    recentNews.value = processedData.recentNews;
+    headerNews.value = processedData.headerNews;
+    newsByCategory.value = processedData.newsByCategory;
+  }
 });
 
-if (status.value === "success" && data.value) {
-  const processedData = processNewsData(data.value.categories, data.value.news);
-  heroNews.value = processedData.heroNews;
-  recentNews.value = processedData.recentNews;
-  headerNews.value = processedData.headerNews;
-  newsByCategory.value = processedData.newsByCategory;
-}
+const isLoading = computed(() => status.value !== "success");
 
-const isLoading = computed(() => status.value === "pending");
+onMounted(() => {
+  execute();
+});
 </script>
